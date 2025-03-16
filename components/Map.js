3615b03c6206195e2f6b43f774fe34a169d09514
IsuruKavi <isurukavindalakshan863@gmail.com>
2025-03-16 23:50:56 +0530

    Add map routes

--- components/Map.js
+++ components/Map.js
@@ -1,15 +1,17 @@
 import { View, Text } from "react-native";
 import React from "react";
-import MapView, { Marker } from "react-native-maps";
+import MapView, { Marker, Polyline } from "react-native-maps";
 import tw from "twrnc";
 import { useSelector } from "react-redux";
 import { selectDestination, selectOrigin } from "../slices/navSlice";
-import MapViewRoute from "react-native-maps-routes";
+import MapViewDirections from "react-native-maps-directions";
 
+import { MapViewRoute } from "react-native-maps-routes";
 const Map = () => {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
-  console.log(origin, destination, process.env.GOOGLE_MAP_API_KEY);
+  const origin1 = { latitude: 37.33228, longitude: -122.01098 };
+  const destination1 = { latitude: 37.423199, longitude: -122.084068 };
 
   return (
     <MapView
@@ -22,8 +24,30 @@ const Map = () => {
         longitudeDelta: 0.005,
       }}
     >
-      {origin && destination && (
-        <MapViewRoute
+      {origin?.location && (
+        <Marker
+          coordinate={{
+            latitude: origin.location.lat,
+            longitude: origin.location.lng,
+          }}
+          description={origin?.description || ""}
+          identifier="origin"
+          title="Origin"
+        />
+      )}
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          description={destination?.description || ""}
+          identifier="destination"
+          title="Destination"
+        />
+      )}
+      {/* {origin && destination && (
+        <MapViewDirections
           apikey={process.env.GOOGLE_MAP_API_KEY}
           strokeWidth={3}
           strokeColors="black"
@@ -36,16 +60,27 @@ const Map = () => {
             longitude: destination.location.lng,
           }}
         />
-      )}
-      {origin?.location && (
-        <Marker
-          coordinate={{
+        
+        
+      )} */}
+
+      {origin && destination && (
+        <MapViewRoute
+          mode="DRIVE"
+          onError={(e) => {
+            console.log(e);
+          }}
+          strokeWidth={4}
+          strokeColor="#00a2ff"
+          origin={{
             latitude: origin.location.lat,
             longitude: origin.location.lng,
           }}
-          description={origin?.description || ""}
-          identifier="origin"
-          title="Origin"
+          destination={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          apiKey={process.env.GOOGLE_MAP_API_KEY}
         />
       )}
     </MapView>
